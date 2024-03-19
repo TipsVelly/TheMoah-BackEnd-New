657963cec52093a747b4fc13839e185b18e2ef41
hyojun kim <37866182+gimyojun@users.noreply.github.com>
2024-03-19 15:54:41 +0900

    first commit

---
+++ src/main/java/com/themoah/themoah/TheMoahApplication.java
@@ -0,0 +1,13 @@
+package com.themoah.themoah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMoahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheMoahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/themoah/themoah/TheMoahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themoah.themoah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMoahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
