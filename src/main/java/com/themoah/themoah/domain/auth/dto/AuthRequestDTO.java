54b2ec90909e1c86941fea302af70c4902a89628
이재민 <leehopl2@gmail.com>
2024-03-21 10:03:43 +0900

    권한 등록 구현

---
+++ src/main/java/com/themoah/themoah/domain/auth/dto/AuthRequestDTO.java
@@ -0,0 +1,20 @@
+package com.themoah.themoah.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequestDTO {
+
+    private String authNm;
+
+    private List<SubAuthRequestDTO>  authRequestDTO;
+
+}
