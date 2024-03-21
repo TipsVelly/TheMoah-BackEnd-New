e06d141b4af69834c331a8f579018514946be9af
JaeMin001 <77082611+JaeMin001@users.noreply.github.com>
2024-03-21 10:06:05 +0900

    권한 등록 구현 (#7)
    Co-authored-by: 이재민 <leehopl2@gmail.com>

--- src/main/java/com/themoah/themoah/domain/auth/controller/AuthMgmtController.java
+++ src/main/java/com/themoah/themoah/domain/auth/controller/AuthMgmtController.java
@@ -1,21 +1,25 @@
 package com.themoah.themoah.domain.auth.controller;
 
-import com.themoah.themoah.domain.auth.entity.AuthGroup;
+import com.themoah.themoah.domain.auth.dto.AuthRequestDTO;
 import com.themoah.themoah.domain.auth.service.AuthMgmtService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/auth")
+@RequiredArgsConstructor
+@RequestMapping(value ="/api/v1/authMgmt", produces = APPLICATION_JSON_VALUE)
 public class AuthMgmtController {
-    private AuthMgmtService authMgmtService;
+    private final AuthMgmtService authMgmtService;
 
-    @GetMapping("/list")
-    public List<AuthGroup> authList(){
-        return authMgmtService.authList();
+    @PostMapping
+    @Operation(summary = "권한 저장 요청(사용자에 따른 기능은 추가로 작업 필요합니다)")
+    public ResponseEntity<?> saveAuth(@RequestBody AuthRequestDTO authRequestDTO) {
+        authMgmtService.saveAuth(authRequestDTO);
+        return ResponseEntity.ok().build();
     }
 }
